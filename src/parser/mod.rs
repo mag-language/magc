70ab649f24001bd10bf4354cd0bc1956511c7a7c
Moritz Riede <moritz@mriede.com>
2022-04-03 01:53:56 +0200

    feat: start working on parser module

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,13 @@
+use crate::token::{Token, TokenKind};
+use parselets::PrefixParselet;
+
+use std::collections::HashMap;
+
+pub mod parselets;
+
 /// A parser which turns a linear token stream into a tree of Mag expressions.
-pub struct Parser {
+pub struct Parser<'a> {
     position: usize,
     source: Vec<Token>,
+    prefix_parselets: HashMap<TokenKind, &'a dyn PrefixParselet>,
 }
