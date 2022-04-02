7528508249b0e727f0a411a51e32a9fb971ad9d5
Moritz Riede <moritz@mriede.com>
2022-04-02 03:48:15 +0200

    feat: integrate clap for nice cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 pub mod token;
 pub mod scanner;
 
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// A file containing Mag code.
+    input: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Hello {}!", args.input)
 }
