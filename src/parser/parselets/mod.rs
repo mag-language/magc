9de9fdf415f971123526f546042de44f6bfed10d
Moritz Riede <moritz@mriede.com>
2022-04-04 00:44:34 +0200

    feat: implement prefix parselet trait

--- src/parser/parselets/mod.rs
+++ src/parser/parselets/mod.rs
@@ -6,3 +6,20 @@ pub trait PrefixParselet {
     fn parse<'a>(&mut self, parser: &'a mut Parser, token: Token) -> Expression<'a>;
 }
 
+/// A parselet which converts an identifier token into an expression.
+pub struct IdentifierParselet;
+
+impl PrefixParselet for IdentifierParselet {
+    fn parse<'a>(&mut self, parser: &'a mut Parser, token: Token) -> Expression<'a> {
+        Expression::Identifier(format!("{}", token))
+    }
+}
+
+/// A parselet which converts a token and the following expression into a prefix expression.
+pub struct PrefixOperatorParselet;
+
+impl PrefixParselet for PrefixOperatorParselet {
+    fn parse<'a>(&mut self, parser: &'a mut Parser, token: Token) -> Expression<'a> {
+        Expression::Identifier(format!("{}", token))
+    }
+}
