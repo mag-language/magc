344fa6baaedcfa618a0a21d24122e0bee5ffeec8
Moritz Riede <moritz@mriede.com>
2022-04-13 01:01:25 +0200

    fix: get around trait object issue by using enum

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,9 @@
 use crate::token::{Token, TokenKind};
-use crate::expression::Expression;
-use parselets::PrefixParselet;
+use crate::expression::{Expression, ExpressionKind};
+
+use parselets::{
+    PrefixParselet,
+};
 
 use std::collections::HashMap;
 
@@ -9,26 +12,67 @@ pub mod parselets;
 /// A parser which turns a linear token stream into a tree of Mag expressions.
 pub struct Parser {
     position: usize,
+    prefix_parselets: HashMap<TokenKind, PrefixParselet>,
     source: Vec<Token>,
 }
 
 impl Parser {
     pub fn new(source: Vec<Token>) -> Self {
+        let mut prefix_parselets = HashMap::new();
+
+        prefix_parselets.insert(TokenKind::Identifier, PrefixParselet::IdentifierParselet);
+
         Self {
             position: 0,
+            prefix_parselets,
             source,
         }
     }
 
+    /*pub fn parse(&mut self) -> Result<Vec<Expression<'a>>, ParserError> {
+        let mut expressions = vec![];
+        let mut buffer = TokenBuffer::new(self.source.clone());
+
+        let mut prefix_p = self.prefix_parselets.borrow_mut();
+
+        prefix_p.insert(TokenKind::Identifier, &IdentifierParselet);
+
+        while !self.eof() {
+            {
+                let start_pos = self.position;
+                let kind = self.parse_expression(&mut buffer)?;
+                let end_pos = self.position;
+
+                expressions.push(Expression {
+                    kind,
+                    start_pos,
+                    end_pos,
+                })
+            }
+        }
+
+        Ok(expressions)
+    }*/
+
     pub fn parse_expression<'a>(
-        &mut self, 
-        mut prefix_parselets: HashMap<&'a TokenKind, &'a mut dyn PrefixParselet>
-    ) -> Result<Expression, ParserError> {
+        &mut self,
+        mut buffer: &'a mut TokenBuffer,
+    ) -> Result<Expression<'a>, ParserError> {
         let token = self.source[self.position].clone();
+        let start_pos = self.position;
 
         {
-            if let Some(prefix) = prefix_parselets.get_mut(&token.kind) {
-                Ok(prefix.parse(self, token))
+            println!("searching prefix parselet for token: {:?}", &token.kind);
+
+            if let Some(parselet) = self.prefix_parselets.get(&token.kind) {
+                let kind = parselet.parse(buffer, token);
+                let end_pos = self.position;
+
+                Ok(Expression {
+                    kind,
+                    start_pos,
+                    end_pos,
+                })
             } else {
                 Err(ParserError::MissingPrefixParselet)
             }
@@ -51,6 +95,7 @@ impl Parser {
     }
 }
 
+#[derive(Debug, Clone)]
 pub enum ParserError {
     MissingPrefixParselet,
 }
@@ -59,4 +104,39 @@ pub struct ParserBuffer {
     items: Vec<Token>,
     lexeme: Option<String>,
     position: usize,
+}
+
+/// A parser-specific buffer structure that can be passed around so
+/// we don't need to reference the parser instance in the parselets.
+pub struct TokenBuffer {
+    pub source: Vec<Token>,
+    pub position: usize,
+}
+
+impl TokenBuffer {
+    pub fn new(source: Vec<Token>) -> Self {
+        Self {
+            source,
+            position: 0,
+        }
+    }
+
+    /// Advance the pointer by one if we're not at the end.
+    fn advance(&mut self) {
+        if !self.eof() {
+            self.position += 1;
+        }
+    }
+
+    fn current(&self) -> Token {
+        self.source[self.position].clone()
+    }
+
+    fn peek(&self) -> Token {
+        self.source[self.position + 1].clone()
+    }
+
+    fn eof(&self) -> bool {
+        self.position >= self.source.len()
+    }
 }
