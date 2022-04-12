b8e33d046d9d396ee5d86c487357be7dddc4cc25
Moritz Riede <moritz@mriede.com>
2022-04-13 01:01:54 +0200

    fix: add enum-based implementation of prefix parselets

--- src/parser/parselets/mod.rs
+++ src/parser/parselets/mod.rs
@@ -1,25 +1,41 @@
-use crate::parser::Parser;
-use crate::expression::Expression;
+use crate::parser::{Parser, TokenBuffer};
+use crate::expression::{Expression, ExpressionKind};
 use crate::token::Token;
 
-pub trait PrefixParselet {
-    fn parse<'a>(&mut self, parser: &'a mut Parser, token: Token) -> Expression<'a>;
+pub enum PrefixParselet {
+    IdentifierParselet,
+}
+
+impl PrefixParselet {
+    pub fn parse<'a>(&self, buffer: &'a mut TokenBuffer, token: Token) -> ExpressionKind<'a> {
+        match self {
+            Self::IdentifierParselet => self.parse_identifier(buffer, token),
+        }
+    }
+
+    fn parse_identifier<'a>(&self, buffer: &'a mut TokenBuffer, token: Token) -> ExpressionKind<'a> {
+        ExpressionKind::Identifier(token.lexeme)
+    }
+}
+
+/*pub trait PrefixParselet {
+    fn parse<'a>(&self, buffer: &'a mut TokenBuffer, token: Token) -> ExpressionKind<'a>;
 }
 
 /// A parselet which converts an identifier token into an expression.
 pub struct IdentifierParselet;
 
 impl PrefixParselet for IdentifierParselet {
-    fn parse<'a>(&mut self, parser: &'a mut Parser, token: Token) -> Expression<'a> {
-        Expression::Identifier(format!("{}", token))
+    fn parse<'a>(&self, buffer: &'a mut TokenBuffer, token: Token) -> ExpressionKind<'a> {
+        ExpressionKind::Identifier(token.lexeme)
     }
-}
+}*/
 
-/// A parselet which converts a token and the following expression into a prefix expression.
-pub struct PrefixOperatorParselet;
+// /// A parselet which converts a token and the following expression into a prefix expression.
+/*pub struct PrefixOperatorParselet;
 
 impl PrefixParselet for PrefixOperatorParselet {
-    fn parse<'a>(&mut self, parser: &'a mut Parser, token: Token) -> Expression<'a> {
-        Expression::Identifier(format!("{}", token))
+    fn parse<'a>(&mut self, parser: &'a mut Parser, token: Token) -> ExpressionKind<'a> {
+        ExpressionKind::Identifier(format!("{}", token))
     }
-}
+}*/
