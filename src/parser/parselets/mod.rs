6ace97ba293c4f3098f238266cdad18d9bcaeeab
Moritz Riede <moritz@mriede.com>
2022-04-17 03:13:32 +0200

    feat: add support for infix parsing

--- src/parser/parselets/mod.rs
+++ src/parser/parselets/mod.rs
@@ -5,17 +5,22 @@ use crate::expression::{
     Expression,
     ExpressionKind,
     PrefixExpression,
+    InfixExpression,
 };
 
 pub trait PrefixParselet {
-    fn parse<'a>(&self, parser: &'a mut Parser, token: Token) -> Expression<'a>;
+    fn parse(&self, parser: &mut Parser, token: Token) -> Expression;
+}
+
+pub trait InfixParselet {
+    fn parse(&self, parser: &mut Parser, left: Box<Expression>, token: Token) -> Expression;
 }
 
 /// A parselet which converts an identifier token into an expression.
 pub struct IdentifierParselet;
 
 impl PrefixParselet for IdentifierParselet {
-    fn parse<'a>(&self, parser: &'a mut Parser, token: Token) -> Expression<'a> {
+    fn parse(&self, parser: &mut Parser, token: Token) -> Expression {
         Expression {
             kind:      ExpressionKind::Identifier,
             lexeme:    token.lexeme,
@@ -28,7 +33,7 @@ impl PrefixParselet for IdentifierParselet {
 pub struct LiteralParselet;
 
 impl PrefixParselet for LiteralParselet {
-    fn parse<'a>(&self, parser: &'a mut Parser, token: Token) -> Expression<'a> {
+    fn parse(&self, parser: &mut Parser, token: Token) -> Expression {
         let kind = match token.kind {
             TokenKind::Literal(literal) => ExpressionKind::Literal(literal),
             _ => unreachable!(),
@@ -47,7 +52,7 @@ impl PrefixParselet for LiteralParselet {
 pub struct PrefixOperatorParselet;
 
 impl PrefixParselet for PrefixOperatorParselet {
-    fn parse<'a>(&self, parser: &'a mut Parser, token: Token) -> Expression<'a> {
+    fn parse(&self, parser: &mut Parser, token: Token) -> Expression {
         let operator = token.clone();
         // TODO: temporary unwrap until we have proper error handling here
         let expr     = parser.parse_expression().unwrap();
@@ -60,6 +65,30 @@ impl PrefixParselet for PrefixOperatorParselet {
             start_pos: 0,
             end_pos: 0,
             lexeme: format!("{}", token.lexeme),
+        }
+    }
+}
+
+pub struct InfixOperatorParselet;
+
+impl InfixParselet for InfixOperatorParselet {
+    fn parse(&self, parser: &mut Parser, left: Box<Expression>, token: Token) -> Expression {
+        println!("[P] infix parselet, left: {:?}, token: {:?}", left, token.clone());
+        parser.advance();
+        // TODO: temporary unwrap until we have proper error handling here
+        let right = parser.parse_expression().unwrap();
+
+        println!("[P] infix parselet, right: {:?}", right);
+
+        Expression {
+            kind: ExpressionKind::Infix(InfixExpression {
+                left,
+                operator: token.clone(),
+                right: Box::new(right),
+            }),
+            lexeme:    token.lexeme,
+            start_pos: token.start_pos,
+            end_pos:   token.end_pos,
         }
     }
 }
