98f7a89f710b6f0b8f94a91572f98f13177e174d
Moritz Riede <moritz@mriede.com>
2022-04-20 14:11:26 +0200

    doc: add more inline documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
+//! A compiler library which provides various methods and data structures used to compile Mag programs 
+//! for a set of back-end architectures, including Strontium bytecode.
+//!
+//! ## Data Structures
+//!
+//! * [`Token`]: The smallest possible entity in the language, like a number, an identifier, or a keyword.
+//! * [`Expression`]: A syntactic entity that may be evaluated to determine its value.
+//!
+//! ## Compilation Pipeline
+//!
+//! Processing starts with the [`Lexer`] struct, which converts a UTF-8 source string to a linear sequence 
+//! of [`Token`]s while annotating them with positioning data and their original text. This enables us to 
+//! iterate easily over our source code in later stages of the pipeline.
+//!
+//! The resulting array of tokens is then fed into a [`Parser`] instance, which attempts to build a tree 
+//! of expressions
+
 pub mod token;
 pub mod lexer;
 pub mod expression;
 pub mod parser;
+
+pub use self::token::Token;
+pub use self::lexer::Lexer;
+pub use self::expression::Expression;
 
 /*use clap::Parser;
 
