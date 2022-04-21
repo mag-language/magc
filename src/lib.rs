e6964c3899179e5e1e766bdd97c0e3f4b35556be
Moritz Riede <moritz@mriede.com>
2022-04-22 00:56:49 +0200

    doc: write more about the compiler pipeline

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,18 @@
 //! ## Compilation Pipeline
 //!
 //! Processing starts with the [`Lexer`] struct, which converts a UTF-8 source string to a linear sequence 
-//! of [`Token`]s while annotating them with positioning data and their original text. This enables us to 
-//! iterate easily over our source code in later stages of the pipeline.
+//! of [`Token`]s while annotating them with positioning data and the original string. This enables us to 
+//! iterate easily over our program in later stages of the pipeline and frees us from having to deal
+//! with strings during parsing. Literals and similar token kinds don't actually contain their values,
+//! instead the lexeme string is stored in the token. This enables us to lazily decode their values, so we
+//! can store the structure in a HashMap without issues. `f64` does not implement `Hash`.
 //!
-//! The resulting array of tokens is then fed into a [`Parser`] instance, which attempts to build a tree 
-//! of expressions
+//! The resulting array of tokens is fed into a [`Parser`] instance and then iteratively turned into a tree 
+//! of expressions representing the structure of the original source string. In case the compiler encounters 
+//! some form of invalid code, it will return an error which is then reported by the runtime.
+//!
+//! This tree of expressions can then be executed on an interpreter or compiled to some binary format like
+//! Strontium bytecode. There is no code generation or interpreter code yet.
 
 pub mod token;
 pub mod lexer;
