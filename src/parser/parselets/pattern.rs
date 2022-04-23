300a3f3035063d5d7a378aa483da01e6a60ae6c8
Moritz Riede <moritz@mriede.com>
2022-04-23 03:34:28 +0200

    test: add tests for variable pattern

--- src/parser/parselets/pattern.rs
+++ src/parser/parselets/pattern.rs
@@ -61,3 +61,89 @@ impl PrefixParselet for VariablePatternParselet {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::lexer::*;
+
+    #[test]
+    fn type_and_name() {
+        let mut lexer = Lexer::new("name String");
+        let tokens = lexer.parse();
+        let mut parser = Parser::new(tokens);
+
+        assert_eq!(
+            Ok(Expression {
+                kind:      ExpressionKind::Pattern(Pattern::Variable {
+                    name: Some("name".to_string()),
+                    type_id: Some("String".to_string()),
+                }),
+                lexeme:    "name".to_string(),
+                start_pos: 0,
+                end_pos:   4,
+            }),
+            parser.parse_expression(0)
+        );
+    }
+
+    #[test]
+    fn no_type_and_no_name() {
+        let mut lexer = Lexer::new("_");
+        let tokens = lexer.parse();
+        let mut parser = Parser::new(tokens);
+
+        assert_eq!(
+            Ok(Expression {
+                kind:      ExpressionKind::Pattern(Pattern::Variable {
+                    name: None,
+                    type_id: None,
+                }),
+                lexeme:    "_".to_string(),
+                start_pos: 0,
+                end_pos:   1,
+            }),
+            parser.parse_expression(0)
+        );
+    }
+
+    #[test]
+    fn type_but_no_name() {
+        let mut lexer = Lexer::new("_ Int");
+        let tokens = lexer.parse();
+        let mut parser = Parser::new(tokens);
+
+        assert_eq!(
+            Ok(Expression {
+                kind:      ExpressionKind::Pattern(Pattern::Variable {
+                    name: None,
+                    type_id: Some("Int".to_string()),
+                }),
+                lexeme:    "_".to_string(),
+                start_pos: 0,
+                end_pos:   1,
+            }),
+            parser.parse_expression(0)
+        );
+    }
+
+    #[test]
+    fn name_but_no_type() {
+        let mut lexer = Lexer::new("lexer");
+        let tokens = lexer.parse();
+        let mut parser = Parser::new(tokens);
+
+        assert_eq!(
+            Ok(Expression {
+                kind:      ExpressionKind::Pattern(Pattern::Variable {
+                    name: Some("lexer".to_string()),
+                    type_id: None,
+                }),
+                lexeme:    "lexer".to_string(),
+                start_pos: 0,
+                end_pos:   5,
+            }),
+            parser.parse_expression(0)
+        );
+    }
+}
