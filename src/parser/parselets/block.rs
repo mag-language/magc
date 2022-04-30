3067b52899105ce2274715000267cec6653ba465
Moritz Riede <moritz@mriede.com>
2022-04-30 03:53:57 +0200

    feat: create block parselet

---
+++ src/parser/parselets/block.rs
@@ -0,0 +1,31 @@
+//! A first-class chunk of code that can be passed around as a value.
+
+use crate::parser::{Parser, ParserResult, PREC_UNARY, PrefixParselet};
+use crate::types::{Expression, ExpressionKind, Prefix, Token};
+
+/// A parselet which converts a token and the following expression into a prefix expression.
+pub struct BlockParselet;
+
+impl PrefixParselet for BlockParselet {
+    fn parse(&self, parser: &mut Parser, token: Token) -> ParserResult {
+        let expressions;
+
+        while !parser.eof() {
+            match parser.peek()?.kind {
+                TokenKind::Keyword(Keyword::End) => {
+                    parser.advance();
+                    break
+                },
+
+                _ => expressions.push(parser.parse_expression(0)?),
+            };
+        }
+
+        Ok(Expression {
+            kind: ExpressionKind::Block(expressions),
+            start_pos: 0,
+            end_pos: 0,
+            lexeme: format!("{}", token.lexeme),
+        })
+    }
+}
