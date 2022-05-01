3912f2fb1f5887fac00ff081a24bcc1214421395
Moritz Riede <moritz@mriede.com>
2022-05-01 23:09:54 +0200

    chore: update type system definition

--- src/type_system.rs
+++ src/type_system.rs
@@ -1,5 +1,7 @@
 /// An interface which is implemented by anything that has a type.
 pub trait Typed {
+    /// Can we assign a value of the given type to a variable of this type?
     fn can_assign_from(&self, other: Box<dyn Typed>) -> bool;
-    fn get_member_type(&self, name: String) -> Option<Box<dyn Typed>>;
+    /// Get the type of this object as a string.
+    fn get_type(&self) -> Option<String>;
 }
