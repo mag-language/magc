289d46846a6838c01560acb2fb97a2a91ee51b26
Moritz Riede <moritz@mriede.com>
2022-05-03 00:51:37 +0200

    chore: move pattern impl to expression impl

--- src/types/expression.rs
+++ src/types/expression.rs
@@ -34,6 +34,41 @@ pub enum ExpressionKind {
     Identifier,
 }
 
+
+impl Expression {
+    /// Match this pattern with a reference signature while obeying the precedence rules for patterns.
+    pub fn linearize(&self, reference: Pattern) -> Result<bool, ParserError> {
+        Ok(match self {
+            Pattern::Value { expr }             => self.linearize_value(reference, expr.clone())?,
+            Pattern::Tuple { left, right }      => self.linearize_tuple(reference, left.clone(), right.clone())?,
+            Pattern::Field { name, value }      => self.linearize_field(reference, name.clone(), value.clone())?,
+            Pattern::Variable { name, type_id } => self.linearize_variable(reference, name.clone(), type_id.clone())?,
+        })
+    }
+
+    fn linearize_value(&self, reference: Pattern, given_expr: Box<Expression>) -> Result<bool, ParserError> {
+        if let Pattern::Value { expr } = reference {
+            Ok(expr == given_expr)
+        } else {
+            Ok(false)
+        }
+    }
+
+    fn linearize_tuple(&self, reference: Pattern, left:  Box<Expression>, right: Box<Expression>) -> Result<bool, ParserError> {
+        let mut does_match = false;
+        
+        Ok(false)
+    }
+
+    fn linearize_field(&self, reference: Pattern, name: String, value: Box<Expression>) -> Result<bool, ParserError> {
+        Ok(false)
+    }
+
+    fn linearize_variable(&self, reference: Pattern, name: Option<String>, type_id: Option<String>) -> Result<bool, ParserError> {
+        Ok(false)
+    }
+}
+
 /// An expression with a prefix operator.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Prefix {
@@ -43,7 +78,7 @@ pub struct Prefix {
 
 /// An expression which defines a multimethod.
 ///
-/// This method can be registered to the same name multiple times if the signature is not already
+/// A method can be registered to the same name multiple times if the signature is not already
 /// present. When it is called in the interpreter, we check if the call signature matches with one
 /// of the defined method's signatures, and if it does, execute that function's body.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
