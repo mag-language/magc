75e4809f8f84638d35d6824c98ba27a6f654cf5e
Moritz Riede <moritz@mriede.com>
2022-05-07 03:40:54 +0200

    feat: comment-out pattern enum and create trait interface instead

--- src/types/pattern.rs
+++ src/types/pattern.rs
@@ -6,32 +6,46 @@ use crate::parser::{
     ParserError,
 };
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum Pattern {
-    /// An expression that evaluates to a value.
-    Value {
-        expr: Box<Expression>
-    },
+/*
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    pub enum Pattern {
+        /// An expression that evaluates to a value.
+        Value {
+            expr: Box<Expression>
+        },
 
-    /// A series of patterns separated by commas.
-    ///
-    /// The data structure is recursive since the comma is defined as an infix operator. This may
-    /// look confusing at first, but is fairly easy to work with since you only need to call the
-    /// method parsing the tuple items recursively.
-    Tuple {
-        left:  Box<Expression>,
-        right: Box<Expression>,
-    },
+        /// A series of patterns separated by commas.
+        ///
+        /// The data structure is recursive since the comma is defined as an infix operator. This may
+        /// look confusing at first, but is fairly easy to work with since you only need to call the
+        /// method parsing the tuple items recursively.
+        Tuple {
+            left:  Box<Expression>,
+            right: Box<Expression>,
+        },
 
-    /// A single entity within a record, like `repeats: 4` or `name: n String`.
-    Field {
-        name: String,
-        value: Box<Expression>,
-    },
+        /// A single entity within a record, like `repeats: 4` or `name: n String`.
+        Field {
+            name: String,
+            value: Box<Expression>,
+        },
 
-    /// A variable identifier with optional name and type.
-    Variable {
-        name: Option<String>,
-        type_id: Option<String>,
-    },
+        /// A variable identifier with optional name and type.
+        Variable {
+            name: Option<String>,
+            type_id: Option<String>,
+        },
+    }
+*/
+
+/// A pattern that can be compared with an [`Expression`] to enable complex flow control
+/// and full destructuring pattern matching, which increases the flexibility and 
+/// expressivity within the language by a great degree.
+pub trait Pattern {
+    /// Return an environment containing the extracted, newly-bound variables if the
+    /// match succeeds, or [`None`] if the expression doesn't match with this pattern.
+    fn match_with(
+        &self, 
+        other: Box<Expression>
+    ) -> Option<HashMap<String, Box<Expression>>;
 }
