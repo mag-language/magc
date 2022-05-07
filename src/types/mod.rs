8c47af74f07ae19d831fa3cbff57872015efb9a3
Moritz Riede <moritz@mriede.com>
2022-05-07 04:28:23 +0200

    chore: define environment type

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,6 +2,10 @@ pub mod expression;
 pub mod token;
 pub mod pattern;
 
+use std::collections::HashMap;
+
+pub type Environment = HashMap<String, Box<Expression>>;
+
 pub use self::expression::*;
 pub use self::token::*;
 pub use self::pattern::*;
