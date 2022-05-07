936a24de3ba1ce166f777c1b490d1e649b1cfedb
Moritz Riede <moritz@mriede.com>
2022-05-08 01:25:13 +0200

    chore: make sure pattern trait implements Debug, Clone, Eq and Hash

--- src/types/pattern/mod.rs
+++ src/types/pattern/mod.rs
@@ -2,6 +2,10 @@ use crate::types::{Expression, Environment};
 use crate::parser::ParserError;
 
 use std::collections::HashMap;
+use std::fmt::Debug;
+use std::clone::Clone;
+use std::cmp::Eq;
+use std::hash::Hash;
 
 pub mod field_pattern;
 pub mod tuple_pattern;
@@ -16,7 +20,7 @@ use self::variable_pattern::*;
 /// A pattern that can be compared with an [`Expression`] to enable complex flow control
 /// and full destructuring pattern matching, which increases the flexibility and 
 /// expressivity within the language by a great degree.
-pub trait Pattern {
+pub trait Pattern: Debug + Clone + Eq + Hash {
     /// Return an environment containing the extracted, newly-bound variables if the
     /// match succeeds, or [`None`] if the expression doesn't match with this pattern.
     fn match_with(
