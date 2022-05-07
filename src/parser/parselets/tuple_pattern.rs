209532d57701fafed6236828527fb57f3d939936
Moritz Riede <moritz@mriede.com>
2022-05-07 06:03:44 +0200

    feat: implement tuple parsing using new trait interface

--- src/parser/parselets/tuple_pattern.rs
+++ src/parser/parselets/tuple_pattern.rs
@@ -31,11 +31,13 @@ impl TuplePatternParselet {
 
 impl PrefixParselet for TuplePatternParselet {
     fn parse(&self, parser: &mut Parser, token: Token) -> ParserResult {
-        let content = parser.parse_expression(0)?;
+        let child = Box::new(self.expect_pattern(parser.parse_expression(0)?)?);
         parser.consume_expect(TokenKind::RightParen);
 
         Ok(Expression {
-            kind: ExpressionKind::Group(Box::new(content)),
+            kind: ExpressionKind::Pattern(
+                Box::new(TuplePattern { child }) as Box<dyn Pattern>,
+            ),
             lexeme:    token.lexeme,
             start_pos: token.start_pos,
             end_pos:   token.end_pos,
--- src/types/pattern/tuple_pattern.rs
+++ src/types/pattern/tuple_pattern.rs
@@ -5,14 +5,9 @@ use crate::types::{
     Pattern,
 };
 
-/// A series of patterns separated by commas.
-///
-/// The data structure is recursive since the comma is defined as an infix operator. This may
-/// look confusing at first, but is fairly easy to work with since you only need to call the
-/// method parsing the tuple items recursively.
+/// A pattern enclosed in parentheses.
 pub struct TuplePattern {
-    pub left:  Box<dyn Pattern>,
-    pub right: Box<dyn Pattern>,
+    pub child: Box<Pattern>,
 }
 
 impl Pattern for TuplePattern {
