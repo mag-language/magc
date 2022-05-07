38aa5993a8a52847090ed9aeb0d42a64e78e264a
Moritz Riede <moritz@mriede.com>
2022-05-07 04:29:45 +0200

    feat: create value pattern and implement trait

--- src/types/pattern/value_pattern.rs
+++ src/types/pattern/value_pattern.rs
@@ -1,6 +1,24 @@
-use crate::types::{Expression};
+use crate::types::{
+    Environment,
+    Expression,
+    Pattern,
+};
 
 /// An expression that evaluates to a value.
 pub struct ValuePattern {
     pub expression: Box<Expression>,
 }
+
+impl Pattern for ValuePattern {
+    fn match_with(&self, expression: Box<Expression>) -> Option<Environment> {
+        if self.expression == expression {
+            // Return an empty environment since we're not destructuring anything here.
+            // This indicates that the match has succeeded.
+            Some(HashMap::new())
+        } else {
+            // Since the expressions do not match, we simply return [`None`] here.
+            None
+        }
+    }
+}
+
