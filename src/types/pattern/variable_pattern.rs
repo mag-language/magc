be5740292e7137b2cb3c726771a87fdb69c498da
Moritz Riede <moritz@mriede.com>
2022-05-07 04:30:16 +0200

    feat: create variable pattern and implement trait

--- src/types/pattern/variable_pattern.rs
+++ src/types/pattern/variable_pattern.rs
@@ -1,10 +1,34 @@
-//use crate::types::{Expression};
+use crate::types::{
+    Environment,
+    Expression,
+    Pattern,
+};
 
-/// A variable identifier with an optional name and type.
+/// A variable identifier with an optional type annotation.
 ///
-/// The pattern consists of an identifier and an optional following type, and if the 
-/// name happens to be `_`, an underscore character, then the variable has no name.
+/// If the name of the identifier happens to be a single underscore character,
+/// the variable is considered nameless and no destructuring will take place.
 pub struct VariablePattern {
     pub name:    Option<String>,
     pub type_id: Option<String>,
+}
+
+impl Pattern for VariablePattern {
+    fn match_with(&self, expression: Box<Expression>) -> Option<Environment> {
+        // We don't really have a type system yet, so let's ignore the pattern's
+        // type identifier for now and just extract any expression into a new
+        // environment without doing type checks.
+        if let Some(name) = self.name {
+            // Add an entry with this pattern's name and the given expression into
+            // the environment so we can use the variable later.
+            Some(
+                HashMap::new().insert(name, expression)
+            )
+        } else {
+            // Return an empty environment since the name is `_`.
+            Some(
+                HashMap::new()
+            )
+        }
+    }
 }
