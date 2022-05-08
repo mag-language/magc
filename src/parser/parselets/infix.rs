1b5db6d3a37708e1a91b5fb02efb578aceab0fb8
Moritz Riede <moritz@mriede.com>
2022-05-08 05:31:13 +0200

    chore: remove pair code

--- src/parser/parselets/infix.rs
+++ src/parser/parselets/infix.rs
@@ -1,8 +1,17 @@
 use crate::parser::{Parser, ParserResult, InfixParselet};
-use crate::types::{Expression, ExpressionKind, Infix, Pattern, Token, TokenKind};
 
-#[derive(Debug, Clone)]
+use crate::types::{
+    Expression,
+    ExpressionKind,
+    Infix,
+    Pattern,
+    TuplePattern,
+    Token,
+    TokenKind,
+};
+
 /// Parse a binary operator expression like `1 + 2`.
+#[derive(Debug, Clone)]
 pub struct InfixOperatorParselet {
     pub precedence: usize,
 }
@@ -13,28 +22,16 @@ impl InfixParselet for InfixOperatorParselet {
 
         let right = parser.parse_expression(self.precedence)?;
 
-        match token.kind {
-            TokenKind::Comma => Ok(Expression {
-                kind: ExpressionKind::Pattern(Pattern::Tuple {
-                    left,
-                    right: Box::new(right),
-                }),
-                lexeme:    token.lexeme,
-                start_pos: token.start_pos,
-                end_pos:   token.end_pos,
+        Ok(Expression {
+            kind: ExpressionKind::Infix(Infix {
+                left,
+                operator: token.clone(),
+                right: Box::new(right),
             }),
-
-            _ => Ok(Expression {
-                kind: ExpressionKind::Infix(Infix {
-                    left,
-                    operator: token.clone(),
-                    right: Box::new(right),
-                }),
-                lexeme:    token.lexeme,
-                start_pos: token.start_pos,
-                end_pos:   token.end_pos,
-            })
-        }
+            lexeme:    token.lexeme,
+            start_pos: token.start_pos,
+            end_pos:   token.end_pos,
+        })
     }
 
     fn get_precedence(&self) -> usize {
