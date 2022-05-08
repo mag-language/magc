ec2a82d39510d04eb1b3fb8a859571e315956254
Moritz Riede <moritz@mriede.com>
2022-05-08 02:30:07 +0200

    chore: comment out pattern matching impls temporarily

--- src/types/pattern/value_pattern.rs
+++ src/types/pattern/value_pattern.rs
@@ -10,16 +10,18 @@ pub struct ValuePattern {
     pub expression: Box<Expression>,
 }
 
-impl Pattern for ValuePattern {
-    fn match_with(&self, expression: Box<Expression>) -> Option<Environment> {
-        if self.expression == expression {
-            // Return an empty environment since we're not destructuring anything here.
-            // This indicates that the match has succeeded.
-            Some(HashMap::new())
-        } else {
-            // Since the expressions do not match, we simply return [`None`] here.
-            None
+/*
+    impl Pattern for ValuePattern {
+        fn match_with(&self, expression: Box<Expression>) -> Option<Environment> {
+            if self.expression == expression {
+                // Return an empty environment since we're not destructuring anything here.
+                // This indicates that the match has succeeded.
+                Some(HashMap::new())
+            } else {
+                // Since the expressions do not match, we simply return [`None`] here.
+                None
+            }
         }
     }
-}
+*/
 
--- src/types/pattern/variable_pattern.rs
+++ src/types/pattern/variable_pattern.rs
@@ -14,22 +14,24 @@ pub struct VariablePattern {
     pub type_id: Option<String>,
 }
 
-impl Pattern for VariablePattern {
-    fn match_with(&self, expression: Box<Expression>) -> Option<Environment> {
-        // We don't really have a type system yet, so let's ignore the pattern's
-        // type identifier for now and just extract any expression into a new
-        // environment without doing type checks.
-        if let Some(name) = self.name {
-            // Add an entry with this pattern's name and the given expression into
-            // the environment so we can use the variable later.
-            Some(
-                HashMap::new().insert(name, expression)
-            )
-        } else {
-            // Return an empty environment since the name is `_`.
-            Some(
-                HashMap::new()
-            )
+/*
+    impl Pattern for VariablePattern {
+        fn match_with(&self, expression: Box<Expression>) -> Option<Environment> {
+            // We don't really have a type system yet, so let's ignore the pattern's
+            // type identifier for now and just extract any expression into a new
+            // environment without doing type checks.
+            if let Some(name) = self.name {
+                // Add an entry with this pattern's name and the given expression into
+                // the environment so we can use the variable later.
+                Some(
+                    HashMap::new().insert(name, expression)
+                )
+            } else {
+                // Return an empty environment since the name is `_`.
+                Some(
+                    HashMap::new()
+                )
+            }
         }
     }
-}
+*/
