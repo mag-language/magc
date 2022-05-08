3aea647367187ef501e4a37867a90e79084f25cb
Moritz Riede <moritz@mriede.com>
2022-05-08 02:06:43 +0200

    chore: remove unneeded imports

--- src/types/pattern/tuple_pattern.rs
+++ src/types/pattern/tuple_pattern.rs
@@ -1,18 +1,9 @@
 use crate::types::{
-    Environment,
-    Expression,
-    ExpressionKind,
     Pattern,
 };
 
 /// A pattern enclosed in parentheses.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Eq, Hash)]
 pub struct TuplePattern {
-    pub child: Box<dyn Pattern>,
-}
-
-impl Pattern for TuplePattern {
-    fn match_with(&self, expression: Box<Expression>) -> Option<Environment> {
-        None
-    }
+    pub child: Box<Pattern>,
 }
