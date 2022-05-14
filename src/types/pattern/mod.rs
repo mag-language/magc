b4797e42f65eff094a34f15736e07bf96d5ea913
Moritz Riede <moritz@mriede.com>
2022-05-15 01:52:09 +0200

    feat: create dummy implementation of linearization methods

--- src/types/pattern/mod.rs
+++ src/types/pattern/mod.rs
@@ -1,3 +1,10 @@
+use crate::types::{
+    Expression,
+    Environment
+};
+use crate::parser::{ParserError};
+use std::collections::HashMap;
+
 mod field;
 mod tuple;
 mod value;
@@ -10,6 +17,8 @@ pub use self::value::*;
 pub use self::pair::*;
 pub use self::variable::*;
 
+pub type LinearizeResult = Result<HashMap<String, Box<Expression>>, ParserError>;
+
 /// A pattern that can be matched with an [`Expression`] to enable complex flow control
 /// and full destructuring pattern matching, which increases the flexibility and 
 /// expressivity within the language by a great degree.
@@ -17,7 +26,7 @@ pub use self::variable::*;
 pub enum Pattern {
     /// A named pattern, like `repeats: 4` or `name: n String`.
     Field(FieldPattern),
-     /// A pattern enclosed in parentheses.
+    /// A pattern enclosed in parentheses.
     Tuple(TuplePattern),
     /// An expression that evaluates to a value.
     Value(ValuePattern),
@@ -25,4 +34,41 @@ pub enum Pattern {
     Variable(VariablePattern),
     /// A pair of patterns separated by a comma.
     Pair(PairPattern),
+}
+
+impl Pattern {
+    /// Compare this pattern with another and return any destructured variables.
+    ///
+    /// This function recursively calls itself and the respective pattern methods
+    /// to evaluate whether a tree of patterns matches with another. A typeless
+    /// variable matches any value pattern, for example.
+    pub fn linearize(&self, other: Pattern) -> LinearizeResult {
+        match self {
+            Pattern::Field(pattern)    => self.linearize_field(pattern.clone(), other),
+            Pattern::Tuple(pattern)    => self.linearize_tuple(pattern.clone(), other),
+            Pattern::Value(pattern)    => self.linearize_value(pattern.clone(), other),
+            Pattern::Variable(pattern) => self.linearize_variable(pattern.clone(), other),
+            Pattern::Pair(pattern)     => self.linearize_pair(pattern.clone(), other),
+        }
+    }
+
+    fn linearize_field(&self, reference: FieldPattern, other: Pattern) -> LinearizeResult {
+        Ok(HashMap::new())
+    }
+
+    fn linearize_tuple(&self, reference: TuplePattern, other: Pattern) -> LinearizeResult {
+        Ok(HashMap::new())
+    }
+
+    fn linearize_value(&self, reference: ValuePattern, other: Pattern) -> LinearizeResult {
+        Ok(HashMap::new())
+    }
+
+    fn linearize_variable(&self, reference: VariablePattern, other: Pattern) -> LinearizeResult {
+        Ok(HashMap::new())
+    }
+
+    fn linearize_pair(&self, reference: PairPattern, other: Pattern) -> LinearizeResult {
+        Ok(HashMap::new())
+    }
 }
