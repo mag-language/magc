3334ba3c3dd55f12cc22cb7e2674091f4d842250
Moritz Riede <moritz@mriede.com>
2022-05-15 03:56:19 +0200

    chore: comment-out can_assign_from temporarily

--- src/type_system.rs
+++ src/type_system.rs
@@ -2,8 +2,8 @@
 
 /// An interface which is implemented by anything that has a type.
 pub trait Typed {
-    /// Can we assign a value of the given type to a variable of this type?
-    fn can_assign_from(&self, other: Box<dyn Typed>) -> bool;
+    // / Can we assign a value of the given type to a variable of this type?
+    // fn can_assign_from(&self, other: Box<dyn Typed>) -> bool;
     /// Get the type of this object as a string.
     fn get_type(&self) -> Option<String>;
 }
