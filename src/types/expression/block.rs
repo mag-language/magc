56f23f7ce3a8a2edda355b5bea002d469b677291
Moritz Riede <moritz@mriede.com>
2022-05-20 00:31:34 +0200

    fix: use BTreeMap in block definition to prevent storage issues with HashMaps

--- src/types/expression/block.rs
+++ src/types/expression/block.rs
@@ -1,8 +1,10 @@
 use crate::types::*;
 
+use std::collections::BTreeMap;
+
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 /// A first-class chunk of code that can be passed around as a value.
 pub struct Block {
-    pub environment: Environment,
+    pub environment: BTreeMap<String, Expression>,
     pub children:    Vec<Expression>,
 }
