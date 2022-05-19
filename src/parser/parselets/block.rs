5934d92abe0426d5baa4cbd75b03c4bcba6a9b36
Moritz Riede <moritz@mriede.com>
2022-05-20 00:30:36 +0200

    chore: use BTreeMap and rename vec

--- src/parser/parselets/block.rs
+++ src/parser/parselets/block.rs
@@ -1,14 +1,16 @@
 //! A first-class chunk of code that can be passed around as a value.
 
 use crate::parser::{Parser, ParserResult, PrefixParselet};
-use crate::types::{Expression, ExpressionKind, Token, TokenKind, Keyword};
+use crate::types::{Expression, ExpressionKind, Block, Token, TokenKind, Keyword};
+
+use std::collections::BTreeMap;
 
 /// Parse a series of expressions contained in a pair of `do` and `end` keywords.
 pub struct BlockParselet;
 
 impl PrefixParselet for BlockParselet {
     fn parse(&self, parser: &mut Parser, token: Token) -> ParserResult {
-        let mut expressions = vec![];
+        let mut children = vec![];
 
         while !parser.eof() {
             match parser.peek()?.kind {
@@ -17,12 +19,15 @@ impl PrefixParselet for BlockParselet {
                     break
                 },
 
-                _ => expressions.push(parser.parse_expression(0)?),
+                _ => children.push(parser.parse_expression(0)?),
             };
         }
 
         Ok(Expression {
-            kind: ExpressionKind::Block(expressions),
+            kind: ExpressionKind::Block(Block {
+                environment: BTreeMap::new(),
+                children,
+            }),
             start_pos: 0,
             end_pos: 0,
             lexeme: format!("{}", token.lexeme),
