081ee51583acf871d20dc8c34f2d2305458d6710
Moritz Riede <moritz@mriede.com>
2022-06-04 02:20:37 +0200

    feat: add type and type error structs

--- src/type_system.rs
+++ src/type_system.rs
@@ -1,9 +1,19 @@
 //! A set of utilities which enable static type checking in Mag.
 
+use crate::types::{
+    Expression,
+};
+
 /// An interface which is implemented by anything that has a type.
 pub trait Typed {
     // / Can we assign a value of the given type to a variable of this type?
     // fn can_assign_from(&self, other: Box<dyn Typed>) -> bool;
     /// Get the type of this object as a string.
     fn get_type(&self) -> Option<String>;
+}
+
+pub struct Type;
+
+pub enum TypeError {
+    UnexpectedType { expected: String, found: Option<String> },
 }
