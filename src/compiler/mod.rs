b68c6401b9612734e5fd53d23b163c190e70d501
Moritz Riede <moritz@mriede.com>
2022-06-15 01:04:51 +0200

    feat: add more compiler types

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,4 +1,4 @@
-use crate::types::{Expression, VariablePattern};
+use crate::types::{Expression, VariablePattern, Pattern};
 use std::collections::HashMap;
 
 pub type Environment<T> = HashMap<String, T>;
@@ -7,6 +7,23 @@ pub struct Compiler {
     /// The global namespace for variables.
     variables:    Environment<Expression>,
     multimethods: Environment<Multimethod>,
+    infix_ops:    Environment<InfixOperatorDefinition>,
+    prefix_ops:   Environment<PrefixOperatorDefinition>,
+    types:        TypeSystem,
+    errors:       ErrorReporter,
 }
 
+pub struct TypeSystem;
+pub struct ErrorReporter;
 pub struct Multimethod;
+
+pub struct InfixOperatorDefinition {
+    pub precedence: usize,
+    pub signature: Option<Pattern>,
+    pub body: Vec<Expression>,
+}
+
+pub struct PrefixOperatorDefinition {
+    pub signature: Option<Pattern>,
+    pub body: Vec<Expression>,
+}
