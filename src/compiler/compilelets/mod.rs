b7bd42a66971cb87cbbef3ed61c69104da4c3ac2
Moritz Riede <moritz@mriede.com>
2023-10-22 23:58:49 +0200

    feat: add optional target register and export new compilelets

--- src/compiler/compilelets/mod.rs
+++ src/compiler/compilelets/mod.rs
@@ -1,8 +1,23 @@
-use strontium::Instruction;
 use crate::types::{Expression, CompilerResult};
+use strontium::machine::instruction::Instruction;
 
 use super::Compiler;
 
+mod call;
+mod infix;
+mod literal;
+mod value_pattern;
+
+pub use self::call::CallCompilelet;
+pub use self::infix::InfixCompilelet;
+pub use self::literal::LiteralCompilelet;
+pub use self::value_pattern::ValuePatternCompilelet;
+
 pub trait Compilelet {
-    fn parse(&self, parser: &mut Compiler, expr: Expression) -> CompilerResult<Vec<Instruction>>;
+    fn compile(
+        &self,
+        compiler: &mut Compiler,
+        expression: Expression,
+        target_register: Option<String>,
+    ) -> CompilerResult<Vec<Instruction>>;
 }
