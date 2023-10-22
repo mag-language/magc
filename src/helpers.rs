9513a97486291846f172e8a49a876f58bb839f78
Moritz Riede <moritz@mriede.com>
2023-10-23 00:07:44 +0200

    doc: add doc comment

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,9 @@
 use unicode_segmentation::UnicodeSegmentation;
 
-pub fn convert_to_graphemes(input: &'static str) -> Vec<&'static str> {
-    input.graphemes(true).collect::<Vec<&'static str>>()
+/// Split up a Unicode source string into a collection of graphemes.
+///
+/// The grapheme is closest to what would be considered a text character in the human
+/// sense, enabling us handle Unicode variable names and strings with relative ease.
+pub fn convert_to_graphemes(input: String) -> Vec<String> {
+    input.graphemes(true).map(|g| g.to_string()).collect()
 }
