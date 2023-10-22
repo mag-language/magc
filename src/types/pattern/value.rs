96dd54ee94a67dd7ea8dfafacf4aa217afb60b2b
Moritz Riede <moritz@mriede.com>
2023-10-23 00:10:01 +0200

    chore: remove old code and fix warnings

--- src/types/pattern/value.rs
+++ src/types/pattern/value.rs
@@ -1,25 +1,7 @@
-use crate::types::{
-    Expression,
-};
+use crate::types::Expression;
 
 /// An expression that evaluates to a value.
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct ValuePattern {
     pub expression: Box<Expression>,
 }
-
-/*
-    impl Pattern for ValuePattern {
-        fn match_with(&self, expression: Box<Expression>) -> Option<Environment> {
-            if self.expression == expression {
-                // Return an empty environment since we're not destructuring anything here.
-                // This indicates that the match has succeeded.
-                Some(HashMap::new())
-            } else {
-                // Since the expressions do not match, we simply return [`None`] here.
-                None
-            }
-        }
-    }
-*/
-
