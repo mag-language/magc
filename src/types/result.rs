81eab96d8a2d185b2b96a45fd90ac0ea7237f933
Moritz Riede <moritz@mriede.com>
2023-10-23 00:10:50 +0200

    chore: extend error types

--- src/types/result.rs
+++ src/types/result.rs
@@ -1,14 +1,25 @@
 pub type CompilerResult<R> = Result<R, CompilerError>;
 
+#[derive(Debug, Clone)]
 pub enum CompilerError {
+    Generic(String),
     LexerError(LexerError),
     ParserError(ParserError),
 }
 
+#[derive(Debug, Clone)]
 pub enum LexerError {
 
 }
 
+#[derive(Debug, Clone)]
 pub enum ParserError {
-
+    /// An opening brace, bracket or parenthesis is missing its closing counterpart.
+    UnclosedDelimiter,
+    /// The file ended unexpectedly.
+    UnexpectedEof,
+    UnexpectedPattern {
+        expected: String,
+        found:    String,
+    },
 }
