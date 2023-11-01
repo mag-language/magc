d77d5196bc2559c2bc367493d9d2c050b69dde13
Moritz Riede <moritz@mriede.com>
2023-11-01 22:35:32 +0100

    chore: add new error types and impls

--- src/types/result.rs
+++ src/types/result.rs
@@ -1,25 +1,81 @@
-pub type CompilerResult<R> = Result<R, CompilerError>;
+use super::{Expression, ExpressionKind, Token, TokenKind, Pattern};
+
+pub type CompilerResult<T> = Result<T, CompilerError>;
 
 #[derive(Debug, Clone)]
 pub enum CompilerError {
     Generic(String),
-    LexerError(LexerError),
+    /// The given method signature has already been defined for this multimethod.
+    DuplicateMethodSignature { signature: Option<Pattern> },
     ParserError(ParserError),
 }
 
-#[derive(Debug, Clone)]
-pub enum LexerError {
+impl std::fmt::Display for CompilerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let error_description: String = match self {
+            Self::Generic(string) => string.clone(),
+            Self::ParserError(error) => format!("{}", error),
+            Self::DuplicateMethodSignature { .. }
+                => format!("this method signature has already been defined for this multimethod"),
+        };
 
+        write!(f, "{}", error_description)
+    }
 }
 
-#[derive(Debug, Clone)]
+impl From<ParserError> for CompilerError {
+    fn from(e: ParserError) -> Self {
+        CompilerError::ParserError(e)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum ParserError {
-    /// An opening brace, bracket or parenthesis is missing its closing counterpart.
-    UnclosedDelimiter,
-    /// The file ended unexpectedly.
-    UnexpectedEof,
+    MissingPrefixParselet(TokenKind),
+    UnexpectedToken {
+        expected: TokenKind,
+        found:    Token,
+    },
+    UnexpectedEOF,
+    UnexpectedExpression {
+        expected: ExpressionKind,
+        found:    Expression,
+    },
+    UnexpectedType {
+        expected: String,
+        found:    Option<String>,
+    },
     UnexpectedPattern {
         expected: String,
         found:    String,
     },
+    ExpectedPattern,
+    /// The linearization of the two given patterns failed.
+    NoMatch,
+}
+
+impl std::fmt::Display for ParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let error_description = match self {
+            Self::MissingPrefixParselet(kind)
+                => format!("no prefix parselet found for token {:?}", kind),
+            Self::UnexpectedToken { expected, found }
+                => format!("expected token {:?}, found {:?}", expected, found),
+            Self::UnexpectedEOF
+                => format!("expected expression, found end of input"),
+            Self::UnexpectedExpression { expected, found }
+                => format!("expected expression {:?}, found {:?}", expected, found),
+            Self::UnexpectedType { expected, found }
+                => format!("expected type {:?}, found {:?}", expected, found),
+            Self::UnexpectedPattern { expected, found }
+                => format!("expected pattern {:?}, found {:?}", expected, found),
+            Self::ExpectedPattern
+                => format!("expected to find a pattern"),
+            Self::NoMatch
+                => format!("the given patterns do not match"),
+            _ => format!("{:?}", self),
+        };
+
+        write!(f, "{}", error_description)
+    }
 }
