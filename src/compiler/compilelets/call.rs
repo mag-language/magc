8bdd9a6d245d6edae2b3f5a6178dee8dbaf2e194
Moritz Riede <moritz@mriede.com>
2023-11-01 22:29:41 +0100

    fix: handle arithmetic and other methods appropriately

--- src/compiler/compilelets/call.rs
+++ src/compiler/compilelets/call.rs
@@ -17,51 +17,61 @@ impl Compilelet for CallCompilelet {
             let method_name = call.name;
             let signature = call.signature.unwrap();
 
-            let method = match method_name.as_str() {
-                "+" => CalculationMethod::ADD,
-                "-" => CalculationMethod::SUBTRACT,
-                "*" => CalculationMethod::MULTIPLY,
-                "/" => CalculationMethod::DIVIDE,
-                "^" => CalculationMethod::POWER,
-                "%" => CalculationMethod::MODULO,
-                _ => unreachable!(),
-            };
+            match method_name.as_str() {
+                // Built-in arithmetic operators
+                "+" | "-" | "*" | "/" | "^" | "%" => {
+                    let method = match method_name.as_str() {
+                        "+" => CalculationMethod::ADD,
+                        "-" => CalculationMethod::SUBTRACT,
+                        "*" => CalculationMethod::MULTIPLY,
+                        "/" => CalculationMethod::DIVIDE,
+                        "^" => CalculationMethod::POWER,
+                        "%" => CalculationMethod::MODULO,
+                        _ => unreachable!(),
+                    };
 
-            if let Pattern::Pair(pair) = signature {
-                let left_expr = if let Pattern::Value(ValuePattern { expression }) = *pair.left {
-                    *expression
-                } else {
-                    unreachable!()
-                };
+                    if let Pattern::Pair(pair) = signature {
+                        let left_expr = if let Pattern::Value(ValuePattern { expression }) = *pair.left {
+                            *expression
+                        } else {
+                            unreachable!()
+                        };
 
-                let right_expr = if let Pattern::Value(ValuePattern { expression }) = *pair.right {
-                    *expression
-                } else {
-                    unreachable!()
-                };
+                        let right_expr = if let Pattern::Value(ValuePattern { expression }) = *pair.right {
+                            *expression
+                        } else {
+                            unreachable!()
+                        };
 
-                let left_register = compiler.registers.allocate_register();
-                instructions.append(&mut compiler.compile_expression(left_expr, Some(left_register.clone()))?);
+                        let left_register = compiler.registers.allocate_register();
+                        instructions.append(&mut compiler.compile_expression(left_expr, Some(left_register.clone()))?);
 
-                let right_register = compiler.registers.allocate_register();
-                instructions.append(&mut compiler.compile_expression(right_expr, Some(right_register.clone()))?);
+                        let right_register = compiler.registers.allocate_register();
+                        instructions.append(&mut compiler.compile_expression(right_expr, Some(right_register.clone()))?);
 
-                let destination_register = target_register.unwrap_or_else(|| compiler.registers.allocate_register());
-                instructions.push(Instruction::CALCULATE {
-                    method,
-                    operand1: left_register,
-                    operand2: right_register,
-                    destination: destination_register.clone(),
-                });
+                        let destination_register = target_register.unwrap_or_else(|| compiler.registers.allocate_register());
+                        instructions.push(Instruction::CALCULATE {
+                            method,
+                            operand1: left_register,
+                            operand2: right_register,
+                            destination: destination_register.clone(),
+                        });
 
-                // Add the interrupt to print the result if this is at the top level.
-                if compiler.context.recursion_depth == 1 {
-                    instructions.push(Instruction::INTERRUPT {
-                        interrupt: Interrupt {
-                            address: destination_register,
-                            kind: InterruptKind::Print,
-                        },
-                    });
+                        // Add the interrupt to print the result if this is at the top level.
+                        if compiler.context.recursion_depth == 1 {
+                            instructions.push(Instruction::INTERRUPT {
+                                interrupt: Interrupt {
+                                    address: destination_register,
+                                    kind: InterruptKind::Print,
+                                },
+                            });
+                        }
+                    }
+                },
+
+                // Any other method calls
+                _ => {
+
                 }
             }
         }
