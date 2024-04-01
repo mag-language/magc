c6c1a01e39faf184ae69d7e1dbc99a14bb1395be
Moritz Riede <moritz@mriede.com>
2024-04-01 03:48:49 +0200

    chore: add address parameter to constructed instruction

--- src/compiler/compilelets/call.rs
+++ src/compiler/compilelets/call.rs
@@ -75,6 +75,7 @@ impl Compilelet for CallCompilelet {
                         // Add the interrupt to print the result if this is at the top level.
                         if compiler.context.recursion_depth == 1 {
                             instructions.push(Instruction::INTERRUPT {
+                                address: destination_register.clone(),
                                 interrupt: Interrupt {
                                     address: destination_register,
                                     kind: InterruptKind::Print,
